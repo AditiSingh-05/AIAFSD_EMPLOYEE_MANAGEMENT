9d6db3769f42808df30ce4a85b6adc60827871fd
AditiSingh-05 <aditisingh151020@gmail.com>
2026-02-18 14:50:37 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config();
 const connectDB = require('./config/db');
 const employeeRoutes = require('./routes/employeeRoutes');
 const errorHandler = require('./middleware/errorMiddleware');
